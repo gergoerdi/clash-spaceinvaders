3494d7a1a0bce031446a05304acc76d48f434e9d
Dr. ERDI Gergo <gergo@erdi.hu>
2019-06-08 13:16:01 +0800

    We need to allow more inlining with the new Higgledy approach :(

--- Shakefile.hs
+++ Shakefile.hs
@@ -21,7 +21,7 @@ clashProject = ClashProject
     , clashFlags =
         [ "-iclash-utils/src-clash"
         , "-Wno-partial-type-signatures"
-        , "-fclash-inline-limit=10"
+        , "-fclash-inline-limit=30"
         , "-fclash-intwidth=32"
         ]
     , shakeDir = "clash-utils/shake"
