84ceed439c00d7dc65b7a9bc7d5a53e6f3c1753e
Dr. ERDI Gergo <gergo@erdi.hu>
2019-06-21 18:54:21 +0800

    Generalize and document the shifter

--- src-clash/Hardware/Clash/SpaceInvaders/Shifter.hs
+++ src-clash/Hardware/Clash/SpaceInvaders/Shifter.hs
@@ -1,26 +1,48 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Hardware.Clash.SpaceInvaders.Shifter where
 
 import Hardware.Intel8080 (Value)
 import Clash.Prelude
 
--- TODO: rewrite this for more clarity...
+-- | This creates a register containing 2*n bits, with new n-bit
+-- values shifted in from the left. Reading out can be done with any
+-- offset up to n bits.
+--
+-- For example, suppose n ~ 4 and you start with the state
+--
+--   0   n
+--   abcdefgh
+--
+-- and then shift in the new value
+--
+--   wxyz
+--
+-- Then the resulting internal value becomes
+--
+--   0   n
+--   wxyzabcd
+--
+-- Setting the read-out offset to 2, the resulting n-bit value is
+--
+--   yzab
 shifter
-    :: (HiddenClockReset domain gated synchronous)
-    => Signal domain (Maybe Value)
-    -> Signal domain (Maybe Value)
-    -> Signal domain Value
-shifter newAmount newValue = startAt <$> amount <*> value
+    :: forall n. (KnownNat n, 1 <= n, CLog 2 n <= n)
+    => forall domain gated synchronous. (HiddenClockReset domain gated synchronous)
+    => Signal domain (Maybe (Unsigned n))
+    -> Signal domain (Maybe (Unsigned n))
+    -> Signal domain (Unsigned n)
+shifter newOffset newValue = startAt <$> offset <*> value
   where
-    amount = regMaybe 0 $ fmap (bitCoerce @(Unsigned 3) . truncateB) <$> newAmount
+    offset = regMaybe 0 $ fmap (bitCoerce . truncateB @_ @(CLog 2 n) @(n - CLog 2 n)) <$> newOffset
     value = regMaybe 0x0000 $ fmap . shiftIn <$> value <*> newValue
 
-    shiftIn :: Unsigned 16 -> Unsigned 8 -> Unsigned 16
+    shiftIn :: Unsigned (2 * n) -> Unsigned n -> Unsigned (2 * n)
     shiftIn old new = bitCoerce (new, old1)
       where
-        (old1, old2) = bitCoerce old :: (Unsigned 8, Unsigned 8)
+        (old1, _old2) = bitCoerce old :: (Unsigned n, Unsigned n)
 
-    startAt :: Index 8 -> Unsigned 16 -> Unsigned 8
-    startAt idx value = result
+    startAt :: Index n -> Unsigned (2 * n) -> Unsigned n
+    startAt offset value = result
       where
-        (result, _) = bitCoerce shiftedValue :: (Unsigned 8, Unsigned 8)
-        shiftedValue = value `rotateL` fromIntegral idx
+        (result, _) = bitCoerce shiftedValue :: (Unsigned n, Unsigned n)
+        shiftedValue = value `rotateL` fromIntegral offset
