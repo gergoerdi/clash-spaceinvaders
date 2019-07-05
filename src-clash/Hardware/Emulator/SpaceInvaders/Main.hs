4265493fe89f2c620db4008b19c7e81f44f6f231
Dr. ERDI Gergo <gergo@erdi.hu>
2019-07-05 21:04:37 +0800

    Change Space Invaders emulator to use CPU emulator for now

--- src-clash/Hardware/Emulator/SpaceInvaders/Main.hs
+++ src-clash/Hardware/Emulator/SpaceInvaders/Main.hs
@@ -2,7 +2,7 @@
 module Main where
 
 import Hardware.Intel8080
-import Hardware.Clash.Intel8080.CPU
+import Hardware.Emulator.Intel8080.CPU
 import Hardware.Emulator.SpaceInvaders.Shifter
 import Hardware.Emulator.SpaceInvaders.Video
 import Hardware.Emulator.SpaceInvaders.Event
@@ -43,75 +43,51 @@ instance (KnownNat n) => Ix (Unsigned n) where
     index (a, b) x = index (fromIntegral a, fromIntegral b) (fromIntegral x)
     inRange (a, b) x = inRange (fromIntegral a, fromIntegral b) (fromIntegral x)
 
-data IRQ = NewIRQ Value
-         | QueuedIRQ Value
-
-data IOR = MkIOR
-    { mem :: SyncMem Addr Value
-    , readPort :: Port -> IO Value
-    , writePort :: Port -> Value -> IO ()
-    , irq :: IORef (Maybe IRQ)
-    , irqAck :: IORef Bool
-    , portSelect :: IORef (Maybe Value)
-    }
-
-mkIOR :: SyncMem Addr Value -> (Port -> IO Value) -> (Port -> Value -> IO ()) -> IO IOR
-mkIOR mem readPort writePort = do
-    irq <- newIORef Nothing
-    irqAck <- newIORef False
-    portSelect <- newIORef Nothing
-    return MkIOR{..}
-
-interrupt :: IOR -> Unsigned 3 -> IO ()
-interrupt MkIOR{..} v = writeIORef irq $ Just $ NewIRQ rst
-  where
-    rst = bitCoerce (0b11 :: Unsigned 2, v, 0b111 :: Unsigned 3)
-
-cpuIO :: CPU CPUIn CPUState CPUOut () -> RWST IOR () CPUState IO ()
-cpuIO step = do
-    MkIOR{..} <- ask
-    cpuInMem <- lift $ Just <$> do
-        interrupting <- readIORef irqAck
-        readingPort <- readIORef portSelect
-        case readingPort of
-            Just port -> do
-                readPort port
-            _
-              | interrupting -> do
-                  putStrLn "Interrupting!"
-                  irqInstr <- do
-                      req <- readIORef irq
-                      case req of
-                          Just (QueuedIRQ op) -> return op
-                          _ -> return 0x00
-                  writeIORef irq Nothing
-                  printf "IRQ: 0x%02x\n" (fromIntegral irqInstr :: Word8)
-                  return irqInstr
-              | otherwise -> do
-                  readData mem
-    cpuInIRQ <- lift $ do
-        req <- readIORef irq
-        case req of
-            Just (NewIRQ op) -> do
-                writeIORef irq $ Just $ QueuedIRQ op
-                return True
-            _ -> return False
-    s <- get
-    let (CPUOut{..}, s') = runState (runCPU defaultOut step CPUIn{..}) s
-    put s'
-    lift $ latchAddress mem cpuOutMemAddr
-    lift $ if cpuOutPortSelect
-           then do
-               let port = truncateB cpuOutMemAddr
-               printf "Port IO: %02x\n" (fromIntegral port :: Word8)
-               writeIORef portSelect $ Just port
-               traverse_ (writePort port) cpuOutMemWrite
-           else do
-               writeIORef portSelect Nothing
-               traverse_ (writeData mem cpuOutMemAddr) cpuOutMemWrite
-    lift $ writeIORef irqAck cpuOutIRQAck
-    -- lift $ printf "<- %04x\n" (fromIntegral cpuOutMemAddr :: Word16)
-    return ()
+-- cpuIO :: CPU CPUIn CPUState CPUOut () -> RWST IOR () CPUState IO ()
+-- cpuIO step = do
+--     MkIOR{..} <- ask
+--     cpuInMem <- lift $ Just <$> do
+--         interrupting <- readIORef irqAck
+--         readingPort <- readIORef portSelect
+--         case readingPort of
+--             Just port -> do
+--                 readPort port
+--             _
+--               | interrupting -> do
+--                   putStrLn "Interrupting!"
+--                   irqInstr <- do
+--                       req <- readIORef irq
+--                       case req of
+--                           Just (QueuedIRQ op) -> return op
+--                           _ -> return 0x00
+--                   writeIORef irq Nothing
+--                   printf "IRQ: 0x%02x\n" (fromIntegral irqInstr :: Word8)
+--                   return irqInstr
+--               | otherwise -> do
+--                   readData mem
+--     cpuInIRQ <- lift $ do
+--         req <- readIORef irq
+--         case req of
+--             Just (NewIRQ op) -> do
+--                 writeIORef irq $ Just $ QueuedIRQ op
+--                 return True
+--             _ -> return False
+--     s <- get
+--     let (CPUOut{..}, s') = runState (runCPU defaultOut step CPUIn{..}) s
+--     put s'
+--     lift $ latchAddress mem cpuOutMemAddr
+--     lift $ if cpuOutPortSelect
+--            then do
+--                let port = truncateB cpuOutMemAddr
+--                printf "Port IO: %02x\n" (fromIntegral port :: Word8)
+--                writeIORef portSelect $ Just port
+--                traverse_ (writePort port) cpuOutMemWrite
+--            else do
+--                writeIORef portSelect Nothing
+--                traverse_ (writeData mem cpuOutMemAddr) cpuOutMemWrite
+--     lift $ writeIORef irqAck cpuOutIRQAck
+--     -- lift $ printf "<- %04x\n" (fromIntegral cpuOutMemAddr :: Word16)
+--     return ()
 
 main :: IO ()
 main = do
@@ -124,8 +100,7 @@ main = do
     memArr <- newListArray (minBound, maxBound) (fromIntegral <$> memL)
     let mem0 = ram (memArr :: IOArray Addr Value)
     videobuf <- newArray (0, 256 * 224 * 4) 0x00
-    mem <- mkSyncMemory $ mem0 `withVideoMem` videobuf
-    latchAddress mem 0x0000
+    mem <- return $ mem0 `withVideoMem` videobuf
 
     shifter <- shifter
     inputPorts <- inputPorts
@@ -140,16 +115,16 @@ main = do
             0x02 -> writeAmount shifter
             0x04 -> writeValue shifter
             _ -> \_ -> return ()
-    r <- mkIOR mem readPort writePort
+    r <- mkR mem readPort writePort
 
     let frameTime = 1000 `div` screenRefreshRate
 
-    let s = initState
+    let s = mkS
 
     let runSome target s = do
-            let stepsPerTick = 500
+            let stepsPerTick = 1500
             (s, i) <- ($ (s, 0)) $ fix $ \loop (s, i) -> do
-                (s, _) <- execRWST (replicateM_ stepsPerTick $ cpuIO cpu) r s
+                (s, _) <- execRWST (replicateM_ stepsPerTick step) r s
                 now <- ticks
                 (if now < target then loop else return) (s, i + 1)
             printf "1 half-frame at %d KHz\n" (fromIntegral (i * stepsPerTick) * screenRefreshRate * 2 `div` 1000)
@@ -168,9 +143,9 @@ main = do
             target2 = target1 + (frameTime `div` 2)
 
         s <- lift $ runSome target1 s
-        lift $ interrupt r 0x01
+        (s, _) <- lift $ execRWST (interrupt $ RST 0x01) r s
         s <- lift $ runSome target2 s
-        lift $ interrupt r 0x02
+        (s, _) <- lift $ execRWST (interrupt $ RST 0x02) r s
 
         render videobuf
         return s
