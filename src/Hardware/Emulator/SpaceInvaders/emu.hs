762d38f654aa0d772925892ea5095d22c4e6264f
Dr. ERDI Gergo <gergo@erdi.hu>
2020-09-05 14:43:11 +0800

    Clean up the emulator to use retroclash-sim

--- src/Hardware/Emulator/SpaceInvaders/emu.hs
+++ src/Hardware/Emulator/SpaceInvaders/emu.hs
@@ -1,16 +1,16 @@
-{-# LANGUAGE RecordWildCards, DataKinds, BinaryLiterals #-}
+{-# LANGUAGE RecordWildCards, BinaryLiterals, OverloadedStrings, NumericUnderscores #-}
 module Main where
-
-import Hardware.Intel8080
-import Hardware.Emulator.Intel8080.CPU
-import Hardware.Emulator.SpaceInvaders.Shifter
-import Hardware.Emulator.SpaceInvaders.Video
-import Hardware.Emulator.SpaceInvaders.Event
-import Hardware.Emulator.SpaceInvaders.Input
-import Hardware.Emulator.Memory
 
 import Prelude ((^))
 import Clash.Prelude hiding ((!), delay, lift, (^))
+import RetroClash.Sim.SDL
+
+import Hardware.Intel8080
+import Hardware.Intel8080.Model
+import Hardware.Emulator.SpaceInvaders.Shifter
+import Hardware.Emulator.SpaceInvaders.Event
+import Hardware.Emulator.SpaceInvaders.Input
+
 import Control.Monad.RWS
 import Control.Monad.Trans.Maybe
 import qualified Data.ByteString as BS
@@ -47,14 +47,16 @@ main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
 
-    -- romFile <- getDataFileName "image/SpaceInvaders.rom"
-    romFile <- return "image/SpaceInvaders.rom"
-    bs <- BS.unpack <$> BS.readFile romFile
-    let memL = L.take (2 ^ 16) $ bs <> L.repeat 0x00
-    memArr <- newListArray (minBound, maxBound) (fromIntegral <$> memL)
-    let mem0 = ram (memArr :: IOArray Addr Value)
-    videobuf <- newArray (0, 256 * 224 * 4) 0x00
-    mem <- return $ mem0 `withVideoMem` videobuf
+    ram <- do
+        ram <- newArray (0x0000, 0xffff) 0 :: IO (IOArray Addr Value)
+
+        -- img <- getDataFileName "image/SpaceInvaders.rom"
+        romFile <- return "image/SpaceInvaders.rom"
+        img <- BS.readFile romFile
+
+        zipWithM_ (writeArray ram) [0x0000..] (fromIntegral <$> BS.unpack img)
+        return ram
+    buf <- newBufferArray @256 @224
 
     shifter <- shifter
     inputPorts <- inputPorts
@@ -65,45 +67,62 @@ main = do
             0x02 -> readPort2 inputPorts
             0x03 -> readValue shifter
             _ -> return 0x00
-        outPort = \port -> case port of
-            0x02 -> writeAmount shifter
-            0x04 -> writeValue shifter
-            _ -> \_ -> return ()
-    r <- mkR mem inPort outPort
+        outPort = \port x -> case port of
+            0x02 -> writeAmount shifter x >> return 0
+            0x04 -> writeValue shifter x >> return 0
+            _ -> return 0
+    let r = MkR (readArray ram) (writeArray ram) inPort outPort
 
-    let frameTime = 1000 `div` screenRefreshRate
+    let frameTime = fromIntegral $ 1000 `div` screenRefreshRate videoParams
 
     let s = mkS
 
-    let execCPU s act = do
-            (s, _) <- execRWST (runMaybeT act) r s
-            return s
+    let execCPU act = runMaybeT act
 
-    let runSome target s = do
-            let stepsPerTick = 10000
-            (s, i) <- ($ (s, 0)) $ fix $ \loop (s, i) -> do
-                s <- execCPU s (replicateM_ stepsPerTick step)
+    let runSome target = do
+            let stepsPerTick = 50000
+            i <- ($ 0) $ fix $ \loop i -> do
+                execCPU $ replicateM_ stepsPerTick step
                 now <- ticks
-                (if now < target then loop else return) (s, i + 1)
-            printf "1 half-frame at %d KHz\n" (fromIntegral (i * stepsPerTick) * screenRefreshRate * 2 `div` 1000)
-            return s
+                (if now < target then loop else return) $ let i' = i + 1 in i' `seq` i'
+            liftIO $ printf "1 half-frame at %d KHz\n" (fromIntegral (i * stepsPerTick) * screenRefreshRate videoParams * 2 `div` 1000)
+            return ()
 
-    withMainWindow $ \render -> (`runContT` return) $ callCC $ \exit' -> ($ s) $ iterateM_ $ \s -> do
-        let exit = exit' ()
+    void $ execRWST `flip` r `flip` s $
+      withMainWindow videoParams $ \events keyDown -> do
+        guard $ not $ keyDown ScancodeEscape
 
         before <- ticks
-        events <- pollEvents
-        keyEvents <- fmap catMaybes $ forM events $ \event -> forM (userEvent $ eventPayload event) $ \ue -> case ue of
-            Quit -> exit
-            Button state btn -> lift $ sinkEvent inputPorts state btn
+        -- keyEvents <- fmap catMaybes $ forM events $ \event -> forM (userEvent $ eventPayload event) $ \ue -> case ue of
+        --     Quit -> exit
+        --     Button state btn -> lift $ sinkEvent inputPorts state btn
 
         let target1 = before + (frameTime `div` 2)
             target2 = target1 + (frameTime `div` 2)
 
-        s <- lift $ runSome target1 s
-        s <- lift $ execCPU s (interrupt $ RST 0x01)
-        s <- lift $ runSome target2 s
-        s <- lift $ execCPU s (interrupt $ RST 0x02)
+        lift $ runSome target1
+        lift $ execCPU (interrupt $ RST 0x01)
+        lift $ runSome target2
+        lift $ execCPU (interrupt $ RST 0x02)
 
-        render videobuf
-        return s
+        liftIO $ forM_ [0..223] $ \y -> do
+            let readBase = 0x2400 + y * 32
+            let fg = 0xff_ff_ff_ff
+                bg = 0xff_00_00_00
+            forM_ [0..31] $ \x -> do
+                let readAddr = fromIntegral $ readBase + x
+                    writeAddr0 = x * 8
+                pixels <- readArray ram readAddr
+                forM_ [0..7] $ \i ->
+                  writeArray (getArray buf) (writeAddr0 + i, y) $
+                  if testBit pixels i then fg else bg
+        return $ rasterizeBuffer buf
+
+
+videoParams :: VideoParams
+videoParams = MkVideoParams
+    { windowTitle = "Space Invaders"
+    , screenScale = 5
+    , screenRefreshRate = 60
+    , reportFPS = True
+    }
