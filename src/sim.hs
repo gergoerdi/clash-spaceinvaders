30c6aef7c9719205c76534451f6f2fea7ae03bb3
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-01 18:35:02 +0800

    Keep a shadow video buffer

--- src/sim.hs
+++ src/sim.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Clash.Prelude hiding ((!))
+{-# LANGUAGE OverloadedStrings, NumericUnderscores #-}
+import Clash.Prelude
 
 import RetroClash.Sim.IO
 import RetroClash.Sim.SDL
@@ -7,46 +7,42 @@ import Hardware.SpaceInvaders
 import Hardware.SpaceInvaders.Video
 
 import Data.Array.IO
-import Data.Array ((!))
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Word
 
 world
     :: IOArray Word16 (Unsigned 8)
+    -> BufferArray VidX VidY
     -> Maybe VidAddr
     -> Maybe (Unsigned 8)
     -> IO (Maybe (Unsigned 8), Maybe (Index VidY))
-world vid vidAddr vidWrite = do
+world vid vbuf vidAddr vidWrite = do
     vidRead <- traverse (readArray vid . fromIntegral) vidAddr
     case (vidAddr, vidWrite) of
-        (Just addr, Just wr) -> writeArray vid (fromIntegral addr) wr
+        (Just addr, Just wr) -> do
+            writeArray vid (fromIntegral addr) wr
+            let (y, x0) = fromIntegral addr `divMod` 32
+            let fg = 0xff_ff_ff
+                bg = 0x00_00_00
+            forM_ [0..7] $ \i -> do
+                let pixel = bitToBool $ wr!i
+                    color = if pixel then fg else bg
+                writeArray (getArray vbuf) (x0 * 8 + i, y) color
         _ -> return ()
     return (vidRead, Nothing) -- TODO: line
 
 main :: IO ()
 main = do
     vid <- newArray (0, 0x1bff) 0
+    vbuf <- newBufferArray
     sim <- simulateIO_ @System (bundle . uncurry mainBoard . unbundle) (Nothing, Nothing)
 
     withMainWindow videoParams $ \events keyDown -> do
         guard $ not $ keyDown ScancodeEscape
 
-        liftIO $ replicateM_ 2000 $ sim $ uncurry $ world vid
-        rasterizeVideoBuf vid
-
--- TODO: want to use VidAddr as the array index...
-rasterizeVideoBuf :: (MonadIO m) => IOArray Word16 (Unsigned 8) -> m (Rasterizer VidX VidY)
-rasterizeVideoBuf vid = do
-    vidArr <- liftIO $ freeze vid
-    return $ rasterizePattern $ \x y ->
-        let (x1, x2) = x `quotRem` 8
-            addr = fromIntegral x1 + fromIntegral y * 32
-            byte = vidArr ! addr
-
-            fg = (0xe7, 0xc2, 0x51)
-            bg = (0x50, 0x50, 0x50)
-        in if testBit byte (fromIntegral x2) then fg else bg
+        liftIO $ replicateM_ 2000 $ sim $ uncurry $ world vid vbuf
+        return $ rasterizeBuffer vbuf
 
 videoParams :: VideoParams
 videoParams = MkVideoParams
