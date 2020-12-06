3ad5d117fad26e1704164ca198c651f9c99620e9
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-06 10:44:20 +0800

    Create video controller

--- src/Hardware/SpaceInvaders/Video.hs
+++ src/Hardware/SpaceInvaders/Video.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NumericUnderscores, RecordWildCards #-}
+{-# LANGUAGE NumericUnderscores, RecordWildCards, ViewPatterns #-}
 module Hardware.SpaceInvaders.Video where
 
 import Clash.Prelude
@@ -27,10 +27,49 @@ video
        , Signal Dom25 (Maybe (Unsigned 8))
        , Signal Dom25 (Maybe (Index VidY))
        )
-video readAddr write = (delayVGA vgaSync rgb, read, lineEnd)
+video (fromSignal -> cpuAddr) (fromSignal -> write) = (delayVGA vgaSync rgb, cpuRead, lineEnd)
   where
     VGADriver{..} = vgaDriver vga640x480at60
 
-    rgb = fromSignal undefined
-    lineEnd = undefined -- TODO: sync with rgb
-    read = undefined
+    (bufX, bufI) = scale @(VidX `Div` 8) (SNat @8) . fst . scale (SNat @2) . center $ vgaX
+    (bufY, _) = scale @VidY (SNat @2) . center $ vgaY
+
+    visible = fromSignal $ isJust <$> bufX .&&. isJust <$> bufY
+    (_, base) = addressBy (snatToNum (SNat @(VidX `Div` 8))) bufY
+    (newBlock, offset) = addressBy 1 bufX
+
+    vidAddr = base + offset
+    allowCPU = not <$> newBlock
+    addr = mux (delayI False allowCPU) (delayI 0 cpuAddr) vidAddr
+    write' = liftA2 (,) <$> enable allowCPU cpuAddr <*> write
+    load = delayedRam (blockRam1 ClearOnReset (SNat @VidSize) 0) addr (delayI Nothing write')
+
+    newCol = fromSignal $ changed Nothing bufI
+    block = delayedRegister 0x00 $ \block ->
+        mux (delayI False newBlock) load $
+        mux (delayI False newCol) ((`shiftL` 1) <$> block) $
+        block
+
+    lineEnd = pure Nothing -- TODO: sync with rgb
+    cpuRead = toSignal $ enable (delayI False allowCPU) load
+
+    pixel = enable (delayI False visible) $ msb <$> block
+    rgb = maybe frame palette <$> pixel
+
+    frame = (0x30, 0x30, 0x30)
+    palette 0 = (0x00, 0x00, 0x00)
+    palette 1 = (0xff, 0xff, 0xff)
+
+addressBy
+    :: (HiddenClockResetEnable dom, NFDataX coord, NFDataX addr, Num coord, Eq coord, Num addr)
+    => addr
+    -> Signal dom (Maybe coord)
+    -> (DSignal dom 0 Bool, DSignal dom 1 addr)
+addressBy stride coord = (new, addr)
+  where
+    start = fromSignal $ coord .== Just 0
+    new = fromSignal $ changed Nothing coord
+    addr = delayedRegister 0 $ \addr ->
+        mux (delayI False start) 0 $
+        mux new (addr + pure stride) $
+        addr
